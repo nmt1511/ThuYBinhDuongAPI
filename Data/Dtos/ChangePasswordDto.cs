9954c5670954a17298483f7280756012bc984a4e
khanhdo137 <2124801030252@student.tdmu.edu.vn>
2025-07-04 21:15:41 +0700

    Đổi mật khẩu cho khách hàng

---
+++ Data/Dtos/ChangePasswordDto.cs
@@ -0,0 +1,8 @@
+namespace ThuYBinhDuongAPI.Data.Dtos
+{
+    public class ChangePasswordDto
+    {
+        public string OldPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+} 
